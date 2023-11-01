ce2946ba408fd01377ccbee24d2f1cb8dad06808
Muhammad ur Rehman <bunny@Bunny-macbook-m1.local>
2023-11-01 21:44:12 +0500

    model validations added

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,5 @@
 class Menu < ApplicationRecord
   validates :category, presence: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
 end
